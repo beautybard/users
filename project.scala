7d953e64f42d46676b6ccf68fd0361fe93e3cb56
dhuff <dallinhuff@gmail.com>
2024-07-04 00:46:57 -0600

    initial commit

---
+++ project.scala
@@ -0,0 +1,14 @@
+//> using publish.organization co.beautybard
+//> using publish.name users
+//> using publish.version 0.1.0-SNAPSHOT
+
+//> using scala 3.3.3
+//> using dep com.softwaremill.sttp.tapir::tapir-http4s-server:1.10.6
+//> using dep org.http4s::http4s-ember-server:0.23.27
+//> using dep com.softwaremill.sttp.tapir::tapir-prometheus-metrics:1.10.6
+//> using dep com.softwaremill.sttp.tapir::tapir-swagger-ui-bundle:1.10.6
+//> using dep com.softwaremill.sttp.tapir::tapir-json-circe:1.10.6
+//> using dep ch.qos.logback:logback-classic:1.5.6
+//> using test.dep com.softwaremill.sttp.tapir::tapir-sttp-stub-server:1.10.6
+//> using test.dep org.scalatest::scalatest:3.2.18
+//> using test.dep com.softwaremill.sttp.client3::circe:3.9.6
